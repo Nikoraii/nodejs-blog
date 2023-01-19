8471488966c2acf4b59401949634b6926103b4d1
Nikoraii <kolja.st@gmail.com>
2023-01-19 19:50:42 +0100

    Fixed order

--- index.js
+++ index.js
@@ -7,28 +7,29 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const commentRoute = require("./routes/comments");
 
-const http = require("http");
-
-app.listen(process.env.PORT, '0.0.0.0', function(err) {
-    console.log("Started listening on %s", app.url);
-});
-
 dotenv.config();
-app.use(express.json());
-
-app.listen("5000", () => {
-    console.log("Backend is running. v2");
-});
-
-app.get("/", (req, res) => {
-    res.send("Test");
-});
 
 mongoose.set("strictQuery", true);
 
 mongoose.connect(process.env.MONGO_URL, {
 
 }).then(console.log("Connected")).catch((err) => console.log(err));
+
+const http = require("http");
+
+app.use(express.json());
+
+app.listen("5000", () => {
+    console.log("Backend is running. v2");
+});
+
+app.listen(process.env.PORT, '0.0.0.0', function(err) {
+    console.log("Started listening on %s", app.url);
+});
+
+// app.get("/", (req, res) => {
+//     res.send("Test");
+// });
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
