9ccd6b7f97c33b53317c37f929e7f3ec07574474
Nikoraii <kolja.st@gmail.com>
2023-01-19 19:01:43 +0100

    Mongoose config

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
     res.send("Test");
 });
 
+mongoose.set("strictQuery", false);
+
 mongoose.connect(process.env.MONGO_URL, {
 
 }).then(console.log("Connected")).catch((err) => console.log(err));
