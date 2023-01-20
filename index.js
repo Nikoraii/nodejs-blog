4b57191eaa130e1d3db6b5f6aba6afb65dea25b4
Nikoraii <kolja.st@gmail.com>
2023-01-20 18:41:39 +0100

    Final

--- index.js
+++ index.js
@@ -23,13 +23,14 @@ app.listen("5000", () => {
     console.log("Backend is running. v2");
 });
 
-app.listen(process.env.PORT, '0.0.0.0', function(err) {
-    console.log("Started listening on %s", app.url);
-});
-
-// app.get("/", (req, res) => {
-//     res.send("Test");
+// app.listen(process.env.PORT, '0.0.0.0', function(err) {
+//     console.log("Started listening on %s", app.url);
 // });
+
+app.get("/", (req, res) => {
+    res.send("Check the <a target='_blank' href='https://github.com/Nikoraii/nodejs-blog/tree/master'>" +
+    "github</a> repo for more information.");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
